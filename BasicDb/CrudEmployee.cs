ab3630061f0a4d5de9a15dca1f1df7d3e9d7bdee
Angel <ngeljames2409@gmail.com>
2020-12-12 21:22:09 +0530

    final commit

---
+++ BasicDb/CrudEmployee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace BasicDb
+{
+    class CrudEmployee
+
+    {
+        SqlConnection con = null;
+        SqlCommand cmd = null;
+        public SqlConnection EstablishConnection()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+            con = new SqlConnection(cs);
+            return con;
+        }
+        public Boolean Readdata()
+        {
+            bool successflag = false;
+            con = EstablishConnection();
+            cmd = new SqlCommand("SELECT Fullname,Empcode,Mobile,Position FROM Employe", con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr[0] + "   " + rdr[1] + "  " + rdr[2] + "    " + rdr[3]);
+                successflag = true;
+            }
+            con.Close();
+            return successflag;
+        }
+
+    }
+}
---
+++ BasicDb/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string cs = "Data Source=LAPTOP-S7I6BAFA\\SQLEXPRESS ;Initial Catalog=sample;Integrated Security=True;";
+            ////SqlConnection con = new SqlConnection();
+            ////con.ConnectionString = cs;
+            //SqlConnection con = new SqlConnection(cs);
+            ////SqlCommand cmd = new SqlCommand();
+            ////cmd.CommandText = " SELECT * FROM Customers";
+            ////cmd.Connection = con;
+            //SqlCommand cmd = new SqlCommand("SELECT * FROM Customers",con);
+            //con.Open();
+            //SqlDataReader rdr=cmd.ExecuteReader();
+            //while (rdr.Read())
+            //{
+            //    Console.WriteLine(rdr[0] + "   " + rdr[1] + "     " + rdr[2] + "");
+            //}
+            //con.Close();
+            CrudEmployee ce = new CrudEmployee();
+            ce.Readdata();
+            Console.ReadLine();
+
+        }
+    }
+}
